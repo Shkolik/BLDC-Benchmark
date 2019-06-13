dc2ea5c6a7ecdaa6d088216f5485eb6d72370df9
Andrew Shkolik <shkolik@gmail.com>
2019-06-13 14:43:20 +0300

    comments added

--- BLDC-Benchmark.ino
+++ BLDC-Benchmark.ino
@@ -1,3 +1,52 @@
+ /*
+ 
+ 
+		VOLTMETER								 TRIGGER
+		[ ][ ][ ]								[ ][ ][ ]
+		 -  +  -								 S  +  -
+					  +-----+									1602 LCD
+         +------------| USB |------------+					  +-------------+
+         |            +-----+            |					16|	[] 			|
+    B5   | [ ]D13/SCK        MISO/D12[ ] |   B4				15|	[] 			|		+ [] plus
+         | [ ]3.3V           MOSI/D11[ ]~|   B3				14|	[] D7		|	
+         | [ ]V.ref     ___    SS/D10[ ]~|   B2				13|	[] D6		|
+    C0   | [ ]A0       / N \       D9[ ]~|   B1				12|	[] D5		|
+    C1   | [ ]A1      /  A  \      D8[ ] |   B0				11|	[] D4		|
+    C2   | [ ]A2      \  N  /      D7[ ] |   D7				10|	[]			|		= [] select
+    C3   | [ ]A3       \_0_/       D6[ ]~|   D6				9 |	[]			|		
+    C4   | [ ]A4/SDA               D5[ ]~|   D5				8 |	[]			|
+    C5   | [ ]A5/SCL               D4[ ] |   D4				7 | []			|
+         | [ ]A6              INT1/D3[ ]~|   D3				6 |	[] EN		|
+         | [ ]A7              INT0/D2[ ] |   D2				5 |	[] RW [GND]	|		- [] minus
+         | [ ]5V                  GND[ ] |     				4 |	[] RS		|		
+    C6   | [ ]RST                 RST[ ] |   C6				3 |	[] VE		|
+         | [ ]GND   5V MOSI GND   TX1[ ] |   D0				2 |	[] V+		|
+         | [ ]Vin   [ ] [ ] [ ]   RX1[ ] |   D1				1 |	[] GND		|
+         |          [ ] [ ] [ ]          |					  |				|
+         |          MISO SCK RST         |					  |				|
+         | NANO-V3                       |					  +-------------+
+         +-------------------------------+
+
+		 D12 -> LCD 14
+		 D11 -> LCD 13
+		 D10 -> LCD 12
+		 D9  -> LCD 11
+		 D8  -> LCD 6
+		 D7  -> LCD 4
+		 
+		 D6  -> plus button
+		 D5  -> select button
+		 D4	 -> minus button
+		 
+		 D3  -> internal trigger signal
+		 D2  -> external trigger signal (30/10 div)
+		 
+		 A0  -> voltmeter (100\10 div)
+		 A1  -> current sensor
+		 A2  -> tensometer
+		 A3  -> tensometer
+		 A4  -> potensiometer
+*/
 #include <LiquidCrystal.h>
 
 // initialize the library by associating any needed LCD interface pin
