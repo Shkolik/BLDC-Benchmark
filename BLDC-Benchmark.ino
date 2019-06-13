802cb840e7830955c4bcc411f72576cea1c13516
Andrew Shkolik <shkolik@gmail.com>
2019-06-13 15:01:12 +0300

    idle screen added

--- BLDC-Benchmark.ino
+++ BLDC-Benchmark.ino
@@ -66,8 +66,7 @@ volatile int REV;
 byte mode = 0, bladesCount = 1, screen = 1;
 
 //  DEFINE RPM AND MAXIMUM RPM
-float rpm, maxRPM;  
-
+float rpm, maxRPM, lastMaxRPM;  
 
 unsigned long time;         //  DEFINE TIME TAKEN TO COVER ONE REVOLUTION
   
@@ -116,7 +115,7 @@ void BladesSelectLoop()
 	else if(readingMenu != lastMenuState && readingMenu == LOW)
 	{
 		screen = 0;
-    MainScreen(0, 0.0);
+		MainScreen(0, 0.0);
     		
 		if(mode > 0)
 			attachInterrupt(digitalPinToInterrupt(ext_trigger), RPMCount, RISING);     	//  ADD A HIGH PRIORITY ACTION ( AN INTERRUPT)  WHEN THE SENSOR GOES FROM LOW TO HIGH
@@ -187,56 +186,54 @@ void MainScreen(int rpm, float voltage)
   lcd.print((int)(rpm/voltage),DEC);
 }
   
-void MainScreenLoop()
+void IdleScreen(int rpm, float voltage)
 {
-  MainScreen((int)rpm, vin);
-  delay(200);
-    
+	lcd.clear();
+	lcd.print("MAX RPM:    KV:");
+	lcd.setCursor(0, 1);
+	lcd.print(rpm,DEC);  
+	lcd.setCursor(12, 1);
+	lcd.print((int)(rpm/voltage),DEC);
+	delay(2000);
+	lcd.clear();
+	lcd.print("IDLE STATE");
+	lcd.setCursor(0, 1);
+	lcd.print("READY TO MEASURE");
+	delay(2000);
+}
+
+void MainScreenLoop()
+{	
+	vin = ((analogRead(dc_in) * 5.0) / 1024.0) / (R2/(R1+R2)); 
+	if (vin < 0.09) 
+		vin = 0.0;								//statement to quash undesired reading !
+	
 	long currtime = millis();                 // GET CURRENT TIME
-  
-	long idletime = currtime - prevtime - 200;        //  CALCULATE IDLE TIME
-    
-	if(REV >= 5 )                  //  IT WILL UPDATE AFETR EVERY 10 READINGS
-	{   
+  	
+	if((currtime - prevtime) > 5000 || REV < 5)                  //  IF THERE ARE NO READING FOR 5 SEC , THE SCREEN WILL SHOW MAX RPM
+	{
+		lastMaxRPM = maxRPM;
+		maxRPM = 0;
+		IdleScreen(lastMaxRPM, vin)
+		prevtime = currtime;
+	}
+	else
+	{
 		int rev = REV;
 		REV = 0;    
-             
-		rpm = 60000/(millis() - time);
-     
-		rpm = rpm*(rev/bladesCount);
-     
+        
+		rpm = (60000/(millis() - time))*(rev/bladesCount);
      
 		if(rpm > maxRPM)
 			maxRPM = rpm;                             //  GET THE MAX RPM THROUGHOUT THE RUN
     
-		time = millis();                            
-     		
-		vout = (analogRead(dc_in) * 5.0) / 1024.0; // see text
-		vin = vout / (R2/(R1+R2)); 
-		if (vin < 0.09) 
-			vin = 0.0;				//statement to quash undesired reading !
+		time = millis(); 
      
 		MainScreen((int)rpm, vin);
 		delay(500);
 
 		prevtime = currtime;                        // RESET IDLETIME
 	}
-   
-	/*if(idletime > 5000 )                      //  IF THERE ARE NO READING FOR 5 SEC , THE SCREEN WILL SHOW MAX RPM
-	{
-		lcd.clear();
-		lcd.print("MAXIMUM RPM");
-		lcd.setCursor(0, 1);
-		lcd.print((int)maxRPM,DEC);                     // DISPLAY MAX RPM
-		lcd.print("   RPM");
-		delay(2000);
-		lcd.clear();
-		lcd.print("IDLE STATE");
-		lcd.setCursor(0, 1);
-		lcd.print("READY TO MEASURE");
-		delay(2000);
-		prevtime = currtime;
-	}*/	
 }
   
 void RPMCount()                                
@@ -259,7 +256,7 @@ void setup()
 {
 	//Serial.begin(9600);   // GET VALUES USING SERIAL MONITOR
 	
-	lcd.begin(16, 2);     // INITIATE LCD
+	lcd.begin(16, 2);     	// INITIATE LCD
 
     pinMode(minus, INPUT);
 	pinMode(plus, INPUT);
@@ -272,7 +269,7 @@ void setup()
 	
 	pinMode(ledPin, OUTPUT);
 
-    REV = 0;      //  START ALL THE VARIABLES FROM 0 
+    REV = 0;      			//  START ALL THE VARIABLES FROM 0 
 	rpm = 0;
 	time = 0;
      
@@ -281,7 +278,6 @@ void setup()
 
 void loop() 
 {
- 
 	switch(screen)
 	{
 		case 0:
@@ -294,6 +290,4 @@ void loop()
 			BladesSelectLoop();
 			break;
 	}
-
-
 }
